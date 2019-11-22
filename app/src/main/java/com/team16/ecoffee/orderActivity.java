d8b988d0a3874e1dd99445a9b76cb106b2b5e07e
chadji15 <chadji15@ucy.ac.cy>
2019-11-22 20:26:16 +0200

    Order activity

---
+++ app/src/main/java/com/team16/ecoffee/orderActivity.java
@@ -0,0 +1,14 @@
+package com.team16.ecoffee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class orderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+}
