b559a9d96e26bc4403051e1ae40457f3430f5fb2
chadji15 <chadji15@ucy.ac.cy>
2019-11-23 21:09:08 +0200

    Customize drink
    Ta skampoooo mouuuu

--- app/src/main/java/com/team16/ecoffee/orderActivity.java
+++ app/src/main/java/com/team16/ecoffee/orderActivity.java
@@ -2,7 +2,10 @@ package com.team16.ecoffee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class orderActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class orderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+        Button freddoButton = findViewById(R.id.freddo_button);
+        freddoButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(orderActivity.this, customizeDrinkActivity.class));
+            }
+        });
     }
 }
