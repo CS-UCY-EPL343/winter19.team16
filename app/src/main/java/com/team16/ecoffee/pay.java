b98b6ac810cfd5bf9174ff77a1a16011b43355d3
aelia003 <36662733+aelia003@users.noreply.github.com>
2019-11-26 13:08:03 +0200

    tryhard

--- app/src/main/java/com/team16/ecoffee/pay.java
+++ app/src/main/java/com/team16/ecoffee/pay.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Payment extends AppCompatActivity {
+public class pay extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_credit);
+        setContentView(R.layout.activity_pay);
     }
 }
